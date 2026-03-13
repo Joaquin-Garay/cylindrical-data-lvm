f51f3a9599443bed0c8ae39b2e606f32140b6736
Joaquin Garay <joaco.garay97@gmail.com>
2026-03-13 18:05:58 -0400

    Update API. Unversioned file deleted.

--- src/soccer_pattern_recognition/hmm/__init__.py
+++ src/soccer_pattern_recognition/hmm/__init__.py
@@ -4,13 +4,11 @@ from .base import EmissionHMM
 from .gaussian_hmm import GaussianHMM
 from .base_emission import BaseEmission
 from .emissions import GaussianEmission
-from .gaussian_mixture_emission import GaussianMixtureEmission
 from .hmm_two_layer import TwoLayerEmission, TwoLayerHMM
 
 __all__ = [
     "BaseEmission",
     "GaussianEmission",
-    "GaussianMixtureEmission",
     "TwoLayerEmission",
     "TwoLayerHMM",
     "EmissionHMM",
